2da97e7961e5281b027f6c91d86301b05331f08b
Sreejith PS <ps.sreejith@litmus7.com>
2023-01-08 09:20:04 +0530

    third commit

--- HelloTest.java
+++ HelloTest.java
@@ -4,10 +4,10 @@ import static org.junit.Assert.assertEquals;
 public class HelloTest {
     @Test
     public void testHello() {
-        assertEquals("Hello World", hello());
+        assertEquals("Hello World!", hello());
     }
 
     public static String hello() {
-        return "Hello World";
+        return "Hello World!";
     }
 }
